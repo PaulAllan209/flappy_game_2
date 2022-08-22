2c5854161ae513e211771f3194b8acbd5a274591
saladlord123 <dpaulallan15@gmail.com>
2022-08-22 12:11:40 +0800

    also testing commited on desktop

--- Flappy_game_2.py
+++ Flappy_game_2.py
@@ -35,7 +35,7 @@ def draw_circle(circle):
 
 # def pipes(space):changed in website tester
 # TODO: Reserch about collision detection in pymunk
-# committed on desktop just testing
+# desktop commit
 def static_ball(space):
     body = pymunk.Body(body_type=pymunk.Body.STATIC)
     body.position = (150, 600)
